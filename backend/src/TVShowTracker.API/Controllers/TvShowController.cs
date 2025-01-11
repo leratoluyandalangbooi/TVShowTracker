a39d5d671cc48c9516a1e2613664991e4a8432fb
LuyaLerato <luyandalangbooi@gmail.com>
2025-01-11 15:24:00 +0200

    tvshow controller

---
+++ backend/src/TVShowTracker.API/Controllers/TvShowController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TVShowTracker.API.Controllers
+{
+    public class TvShowController : Controller
+    {
+        // POST: importTvShows..this will reaad the api and import the tv shows. this must be authenticated.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
